ad87b8aab237a9b9dee4f26480d77d589896bfbe
shanehull <hello@shanehull.com>
2024-03-07 08:46:13 +1100

    chore: enable noise with which-key

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -52,7 +52,6 @@ return {
 	-- Pretty cmdline
 	{
 		"folke/noice.nvim",
-		enabled = false,
 		tag = "v2.0.0",
 		event = "VeryLazy",
 		dependencies = {
@@ -85,6 +84,36 @@ return {
 				},
 			})
 		end,
+	},
+
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			plugins = {
+				marks = true, -- shows a list of your marks on ' and `
+				registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+				spelling = {
+					enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+					suggestions = 10, -- how many suggestions should be shown in the list?
+				},
+				-- the presets plugin, adds help for a bunch of default keybindings in Neovim
+				-- No actual key bindings are created
+				presets = {
+					operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+					motions = false, -- adds help for motions
+					text_objects = false, -- help for text objects triggered after entering an operator
+					windows = true, -- default bindings on <c-w>
+					nav = true, -- misc bindings to work with windows
+					z = true, -- bindings for folds, spelling and others prefixed with z
+					g = true, -- bindings for prefixed with g
+				},
+			},
+		},
 	},
 
 	-- Icons
