b11d03824e98bf644b7a64b1b6de3a62e419169b
shanehull <shane.hull@annalise.ai>
2024-04-19 12:49:38 +1000

    chore: search hidden files

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -4,6 +4,20 @@ return {
 		"nvim-telescope/telescope.nvim",
 		dependencies = { { "nvim-lua/plenary.nvim" } },
 		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+					grep_string = {
+						additional_args = { "--hidden" },
+					},
+					live_grep = {
+						additional_args = { "--hidden" },
+					},
+				},
+			})
+
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find a file with telescope" })
 			vim.keymap.set("n", "<leader>ft", builtin.live_grep, { desc = "Find text with telescope" })
