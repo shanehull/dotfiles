bba84483f3bc868b78905562063675c7af3d9db8
shanehull <hello@shanehull.com>
2024-05-09 10:22:38 +1000

    fix: lualine issue for lazy updates

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -49,7 +49,17 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		config = function()
-			require("lualine").setup()
+			require("lualine").setup({
+				sections = {
+					lualine_x = {
+						{
+							require("lazy.status").updates,
+							cond = require("lazy.status").has_updates,
+							color = { fg = "#ff9e64" },
+						},
+					},
+				},
+			})
 		end,
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
