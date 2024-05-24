bddbaee8de74be6f9b228d1e61999b4664cdb52e
shanehull <hello@shanehull.com>
2024-05-24 16:34:57 +1000

    chore: add pam-reattach

--- nix/darwinConfigurations/nixos.nix
+++ nix/darwinConfigurations/nixos.nix
@@ -1,66 +1,76 @@
-{ inputs, ... }@flakeContext:
-let
-darwinModule = { config, lib, pkgs, ... }: {
+{inputs, ...} @ flakeContext: let
+  darwinModule = {
+    config,
+    lib,
+    pkgs,
+    ...
+  }: {
     imports = [
-        inputs.home-manager.darwinModules.home-manager
-            inputs.self.homeConfigurations.shed.nixosModule
-            {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                nixpkgs = { config = { allowUnfree = true; }; };
-            }
+      inputs.home-manager.darwinModules.home-manager
+      inputs.self.homeConfigurations.shed.nixosModule
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        nixpkgs = {config = {allowUnfree = true;};};
+      }
     ];
     config = {
-        documentation = {
-            doc = {
-                enable = false;
-            };
+      documentation = {
+        doc = {
+          enable = false;
         };
-        nix = {
-            extraOptions = ''
-                experimental-features = nix-command flakes
-                '';
+      };
+      nix = {
+        extraOptions = ''
+          experimental-features = nix-command flakes
+        '';
+      };
+      programs = {
+        zsh = {
+          enable = true;
         };
-        programs = {
-            zsh = {
-                enable = true;
-            };
+      };
+      services = {
+        nix-daemon = {
+          enable = true;
         };
-        services = {
-            nix-daemon = {
-                enable = true;
-            };
+      };
+      users = {
+        users.shane.home = "/Users/shane";
+      };
+      system = {
+        stateVersion = 4;
+        defaults = {
+          dock = {
+            autohide = true;
+            show-recents = false;
+          };
         };
-        users = {
-            users.shane.home = "/Users/shane";
-        };
-        system = {
-            stateVersion = 4;
-            defaults = {
-                dock = {
-                    autohide = true;
-                    show-recents = false;
-                };
-            };
-        };
-        environment = {
-            shells = [ pkgs.bash pkgs.zsh ];
-            loginShell = pkgs.zsh;
-            systemPackages = [ pkgs.coreutils ];
-        };
-        homebrew = {
-            enable = true;
-            onActivation.autoUpdate = true;
-            casks = [ "warp" "multipass" ];
-            brews = [ "cloudflare-wrangler2" ];
-        };
-        security.pam.enableSudoTouchIdAuth = true;
+      };
+      environment = {
+        shells = [pkgs.bash pkgs.zsh];
+        loginShell = pkgs.zsh;
+        systemPackages = [pkgs.coreutils];
+      };
+      homebrew = {
+        enable = true;
+        onActivation.autoUpdate = true;
+        casks = ["warp" "multipass"];
+        brews = ["cloudflare-wrangler2"];
+      };
+      # this doesn't work with tmux (adds pam_tid in /etc/pam.d/sudo file)
+      security.pam.enableSudoTouchIdAuth = true;
+      # additionally, use pam_reattach, as this works with tmux
+      # pam_reattach is installed via: ../homeConfigurations/packages/default.nix
+      environment.etc."pam.d/sudo_local".text = ''
+        auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so # nix-darwin: environment.etc."pam.d/sudo_local".text
+      '';
     };
-};
+  };
 in
-inputs.nix-darwin.lib.darwinSystem {
+  inputs.nix-darwin.lib.darwinSystem {
     modules = [
-        darwinModule
+      darwinModule
     ];
     system = "aarch64-darwin";
-}
+  }
