e9380fa6b7f4499cc1e02824539637a53f4e3daf
shanehull <shane.hull@annalise.ai>
2024-07-26 11:33:20 +1000

    chore: add wezterm conf

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,44 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.color_scheme = "Gruvbox dark, medium (base16)"
+config.font = wezterm.font("Hack Nerd Font")
+config.window_background_opacity = 0.80
+
+config.default_prog = {
+	"/bin/zsh",
+	"-l",
+}
+
+config.keys = {
+	{
+		key = "h",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "l",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "-",
+		mods = "LEADER",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+}
+
+return config
