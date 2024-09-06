50e689e0da8fc50680d33bfc4ec5e16907df8a70
shanehull <hello@shanehull.com>
2024-09-06 21:11:24 +1000

    core: update gpg key

--- nix/homeConfigurations/programs/default.nix
+++ nix/homeConfigurations/programs/default.nix
@@ -35,7 +35,7 @@
     userEmail = "hello@shanehull.com";
     signing = {
       signByDefault = true;
-      key = "954E6CE09F184BF5";
+      key = "B53433348CA2F9F9";
     };
     extraConfig = {
       init = {
