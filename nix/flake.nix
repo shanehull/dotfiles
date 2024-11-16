e2a3ef20ac1816ad96c66fac9009e878b68df8d9
shanehull <hello@shanehull.com>
2024-11-16 20:20:12 +1100

    chore: formatting

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,24 +1,22 @@
 {
-    description = "Shane's MacOS developer environment.";
-    inputs = {
-        nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
-        nix-darwin.url = "flake:nix-darwin";
-        nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-        home-manager.url = "flake:home-manager";
-        home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  description = "Shane's MacOS developer environment.";
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "flake:nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "flake:home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = inputs: let
+    flakeContext = {
+      inherit inputs;
     };
-    outputs = inputs:
-        let
-        flakeContext = {
-            inherit inputs;
-        };
-    in
-    {
-        darwinConfigurations = {
-            shed = import ./darwinConfigurations/nixos.nix flakeContext;
-        };
-        homeConfigurations = {
-            shed = import ./homeConfigurations/shed.nix flakeContext;
-        };
+  in {
+    darwinConfigurations = {
+      shed = import ./darwinConfigurations/nixos.nix flakeContext;
     };
+    homeConfigurations = {
+      shed = import ./homeConfigurations/shed.nix flakeContext;
+    };
+  };
 }
