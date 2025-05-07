af025f8101ee4c2ca0565d059893ccd2f45aebc0
shanehull <hello@shanehull.com>
2025-05-07 15:19:35 +1000

    chore: disable golines

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -20,7 +20,7 @@ return {
 				sources = {
 					null_ls.builtins.formatting.gofmt,
 					null_ls.builtins.formatting.goimports,
-					null_ls.builtins.formatting.golines,
+					-- null_ls.builtins.formatting.golines,
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.alejandra,
 					null_ls.builtins.formatting.terraform_fmt,
