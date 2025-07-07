795829c55324e0f9911542b17a0cbc373e324092
shanehull <shane.hull@remote.com>
2025-07-07 15:36:09 +1000

    chore: add copilot

--- nvim/lua/plugins/ai-assist.lua
+++ nvim/lua/plugins/ai-assist.lua
@@ -1,31 +1,38 @@
+-- Function to check if a module is available before requiring it.
+local function safe_require(module)
+	local ok, result = pcall(require, module)
+	if ok and type(result) == "table" then
+		return result
+	else
+		return { ENABLE_COPILOT = false }
+	end
+end
+
+-- Load the environment module safely (file can be absent).
+local env = safe_require("env")
+
 return {
 	{
 		"sourcegraph/sg.nvim",
 		config = function()
-			-- Function to check if a module is available before requiring it.
-			local function safe_require(module)
-				local ok, result = pcall(require, module)
-				if ok and type(result) == "table" then
-					return result
-				else
-					return { ALLOW_AI_ASSIST = false }
-				end
-			end
-
-			-- Load the environment module safely (file can be absent).
-			local env = safe_require("env")
-
-			-- Enable cody?
-			local enable_cody = env.ALLOW_AI_ASSIST
-
 			-- Run if enabled and not certain ft
 			runFunc = function()
-				if not vim.tbl_contains({ "markdown" }, vim.bo.ft) and enable_cody then
+				if not vim.tbl_contains({ "markdown" }, vim.bo.ft) and env.ENABLE_CODY then
 					require("sg").setup({})
 				end
 			end
 			vim.defer_fn(runFunc, 50)
 		end,
 		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	},
+	{
+		"github/copilot.vim",
+		config = function()
+			if not vim.tbl_contains({ "markdown" }, vim.bo.ft) and env.ENABLE_COPILOT then
+				vim.g.copilot_enabled = true
+			else
+				vim.g.copilot_enabled = false
+			end
+		end,
 	},
 }
