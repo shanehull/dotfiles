d009d4554aea900b06f20ab4d2cf9487d9e99c39
shanehull <shane.hull@remote.com>
2025-08-04 11:21:11 +1000

    chore: use cond for copilot enable

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -3,20 +3,21 @@ return {
 		"zbirenbaum/copilot.lua",
 		cmd = "Copilot",
 		event = "InsertEnter",
+		cond = function()
+			return vim.env.ENABLE_COPILOT ~= nil and vim.env.ENABLE_COPILOT ~= ""
+		end,
 		config = function()
-			if vim.env.ENABLE_COPILOT == "true" then
-				require("copilot").setup({
-					suggestion = {
-						enabled = false,
-					},
-					panel = {
-						enabled = false,
-					},
-					filetypes = {
-						yaml = true,
-					},
-				})
-			end
+			require("copilot").setup({
+				suggestion = {
+					enabled = false,
+				},
+				panel = {
+					enabled = false,
+				},
+				filetypes = {
+					yaml = true,
+				},
+			})
 		end,
 	},
 }
