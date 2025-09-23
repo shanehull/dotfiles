0b4bd1d362116bb72ec2afec4afefbba394b2f95
shanehull <hello@shanehull.com>
2025-09-23 11:56:56 +1000

    fix: lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -8,81 +8,16 @@ return {
 			vim.g.lsp_zero_extend_lspconfig = 0
 		end,
 	},
-	{ "williamboman/mason.nvim" },
 	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = { "neovim/nvim-lspconfig" },
+		"williamboman/mason.nvim",
 		config = function()
-			local lsp_zero = require("lsp-zero")
-			local lspconfig = require("lspconfig")
-			local mason = require("mason")
-			local mason_lspconfig = require("mason-lspconfig")
-
-			-- Use default but extend
-			lsp_zero.extend_lspconfig()
-
-			-- LSP zero setup
-			lsp_zero.on_attach(function(client, bufnr)
-				local opts = { buffer = bufnr, remap = false }
-
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, opts)
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, opts)
-				vim.keymap.set("n", "<leader>vws", function()
-					vim.lsp.buf.workspace_symbol()
-				end, opts)
-				vim.keymap.set("n", "<leader>vd", function()
-					vim.diagnostic.open_float()
-				end, opts)
-				vim.keymap.set("n", "[d", function()
-					vim.diagnostic.goto_next()
-				end, opts)
-				vim.keymap.set("n", "]d", function()
-					vim.diagnostic.goto_prev()
-				end, opts)
-				vim.keymap.set("n", "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrn", function()
-					vim.lsp.buf.rename()
-				end, opts)
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, opts)
-			end)
-
-			-- Fix Undefined global 'vim'
-			lsp_zero.configure("lua_ls", {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-					},
-				},
-			})
-
-			-- Mason setup - default LSPs
-			mason.setup({})
-			mason_lspconfig.setup({
-				ensure_installed = {
-					"gopls",
-					"golangci_lint_ls",
-					"typos_lsp",
-				},
-				handlers = {
-					lsp_zero.default_setup,
-				},
-			})
-
-			-- Config individual LSPs
-			lspconfig.gopls.setup({
+			require("mason").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("gopls", {
 				settings = {
 					gopls = {
 						analyses = {
@@ -95,17 +30,19 @@ return {
 				},
 			})
 
-			lspconfig.lexical.setup({
+			vim.lsp.config("lexical", {
 				cmd = { "lexical" },
 				root_dir = function(fname)
-					return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.cwd()
+					return vim.lsp.config.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.cwd()
 				end,
 				filetypes = { "elixir", "eelixir", "heex" },
 				-- optional settings
 				settings = {},
 			})
 
-			lspconfig.ocamllsp.setup({})
+			vim.lsp.config("ocamllsp", {
+				fileypes = { "ocaml" },
+			})
 		end,
 	},
 }
