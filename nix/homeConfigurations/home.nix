09c74996aa8f41cff79f1124b0aaadb96289d110
shane.hull <shane.hull@remote.com>
2026-02-04 09:08:08 +1100

    chore: bump custom clis

--- nix/homeConfigurations/home.nix
+++ nix/homeConfigurations/home.nix
@@ -109,19 +109,19 @@
             {
               pname = "shed";
               fname = "shed-darwin-arm64";
-              version = "v0.0.3";
+              version = "v0.1.0";
               repo = "shed";
               owner = "shanehull";
-              sha256 = "sha256-McEzuLE6+xKwNb5PtXrLVb0GyH1hRQSr5yPLvm9WKzQ=";
+              sha256 = "sha256-mZa3K4DX0BoNNeFo9nQkhRgRNxmCJfdfA6RBnN9xHzY=";
             })
           (mkGithubReleasePkg
             {
               pname = "quickval";
               fname = "quickval-darwin-arm64";
-              version = "latest";
+              version = "v1.3.0";
               repo = "quickval";
               owner = "shanehull";
-              sha256 = "sha256-Pxtz5kFfZt24PG6Z19N9obm2mCXwB8z/qdptPqmkZ1o=";
+              sha256 = "sha256-DKzsofCM5YElilMFwvCSHzhjtJPAB/sAIucisbITObo=";
             })
         ];
       };
