683bbb2b096469bf6fbc0c40bcf689717bcc58ff
shane.hull <shane.hull@remote.com>
2026-04-24 07:51:48 +1000

    chore: add font fallback wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,7 +8,10 @@ return {
 	enable_tab_bar = false,
 	window_background_opacity = 0.90,
 	font_size = 12.0,
-	font = wezterm.font("Hack Nerd Font"),
+	font = wezterm.font_with_fallback({
+		"Hack Nerd Font",
+		"Hiragino Sans GB",
+	}),
 
 	set_environment_variables = {
 		TERMINFO_DIRS = "/etc/profiles/per-user/$USER/share/terminfo",
