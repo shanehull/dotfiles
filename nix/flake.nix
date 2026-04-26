15fad4b211aa0682b2c9ca5f576c7846b8db4861
shane.hull <shane.hull@remote.com>
2026-04-27 09:05:49 +1000

    fix: pkgs url

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Shane's MacOS developer environment.";
   inputs = {
-    nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     qmd.url = "github:tobi/qmd";
     nix-darwin.url = "flake:nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
