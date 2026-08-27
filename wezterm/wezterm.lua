32c102028de62ccaf8e0b9f463495a84a8bac846
shanehull <hello@shanehull.com>
2026-08-27 11:34:57 +1000

    feat: enable wezterm tabs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ return {
 	front_end = "OpenGL",
 
 	color_scheme = "Gruvbox dark, medium (base16)",
-	enable_tab_bar = false,
+	enable_tab_bar = true,
 	window_background_opacity = 0.90,
 	font_size = 10,
 	font = wezterm.font_with_fallback({
